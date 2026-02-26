5f75d48403cf500e16bd364db63f8decab159a02
zychen027 <zychen027@163.com>
2026-02-26 13:38:24 +0800

    Fixed Register Error

--- src/main/java/com/packetmine/addon/PacketMineAddon.kt
+++ src/main/java/com/packetmine/addon/PacketMineAddon.kt
@@ -3,14 +3,21 @@ package com.packetmine.addon
 import meteordevelopment.meteorclient.addons.MeteorAddon
 import meteordevelopment.meteorclient.systems.modules.Category
 import meteordevelopment.meteorclient.systems.modules.Modules
+import net.minecraft.item.Items
 
 class PacketMineAddon : MeteorAddon() {
 
     companion object {
-        // 正确创建 Category 实例
-        val PACKETMINE_CATEGORY: Category = Category("PacketMine")
+        // 自定义分类，建议给一个图标 ItemStack
+        val PACKETMINE_CATEGORY: Category = Category("PacketMine", Items.DIAMOND_PICKAXE.defaultStack)
     }
 
+    // 1. 先注册 Category（这是 Meteor 要求的必须步骤）
+    override fun onRegisterCategories() {
+        Modules.registerCategory(PACKETMINE_CATEGORY)
+    }
+
+    // 2. 再在 onInitialize 里注册模块
     override fun onInitialize() {
         Modules.get().add(PacketMineModule())
     }
